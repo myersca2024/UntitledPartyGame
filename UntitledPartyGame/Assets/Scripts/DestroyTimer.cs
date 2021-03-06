68fc6f6642685a1004de4add71b035def6d172eb
myersca2024 <myers.ca@northeastern.edu>
2021-03-05 18:26:45 -0600

    Completely functional AI

---
+++ UntitledPartyGame/Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    public float timeToDestroy;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit player!");
+        }
+        Destroy(gameObject);
+    }
+}

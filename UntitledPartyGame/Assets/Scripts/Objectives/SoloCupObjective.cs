d11d0e9cbd1c47874f3a9cc5432cb88859fa9602
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2021-03-06 22:17:40 -0500

    Solo Cup Objective Scripts (#25)
    * Level Manager and related UI
    
    * Methods and Strings For Each Objective
    
    * Got Rid of Resources Folder
    
    * Noise Level Slider Changes
    
    The color of the slider and text change when the win condition is met
    
    * Solo Cup and Liquor Models
    
    * Half of the Objective Scripts, mostly outlining
    
    * Fixed Transform of Solo Cup and Liquor Bottle
    
    * Solo Cup Objective Scripts

---
+++ UntitledPartyGame/Assets/Scripts/Objectives/SoloCupObjective.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoloCupObjective : MonoBehaviour
+{
+
+    public int numCups = 0;
+
+    LevelManager lv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lv = FindObjectOfType<LevelManager>();
+    }
+
+    public void IncreaseCups()
+    {
+        numCups++;
+    }
+
+    public void DecreaseCups()
+    {
+        numCups--;
+        if (numCups <= 0)
+        {
+            lv.SoloComplete();
+        }
+    }
+}

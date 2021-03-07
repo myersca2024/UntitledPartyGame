d493e40930c7572848ee81c0daa8b6a78695f295
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2021-03-06 20:59:34 -0500

    Half of the Objective Scripts, mostly outlining

---
+++ UntitledPartyGame/Assets/Scripts/Objectives/CallParentsObjective.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallParentsObjective : MonoBehaviour
+{
+
+    LevelManager lv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lv = FindObjectOfType<LevelManager>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Display Interact Dialogue?
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(gameObject.GetComponent<BoxCollider>());
+                lv.ParentsComplete();
+            }
+        }    
+    }
+}

23bec69e94f43f7cfc169e1e544b2f23a53d0eb2
myersca2024 <71057364+myersca2024@users.noreply.github.com>
2021-03-07 22:48:05 -0600

    AAAAA (#78)
    * Added asset pack and probuilder
    
    * AI changes
    
    * Moved reticle script
    
    * Following bugfix
    
    * AI update
    
    * Organization merge
    
    * Added AI attacking
    
    * Complete AI functionality
    
    * Updated scene
    
    * Police AI update (not finished)
    
    * Prefab Bugfix
    
    * Completely functional AI
    
    * Lot of imports
    
    * Also updated reticle
    
    * Final bugfix to throwing
    
    * Bugfixing
    
    * Water and firepit
    
    * Added furniture pack
    
    * Fine-tuning for level prototype
    
    * Added first level prototype
    
    * Added Chad effect to NPCs
    
    * Re-added NPC SFX
    
    * Updated main menu
    
    * Added much functionality to level
    
    * Update prototype
    
    * Added cracked TV
    
    * TV prefab completed
    
    * Updated AI to be hit by bottles
    
    * Added level functionality
    
    * added game over screen
    
    * Phone call functionality
    
    * Finished phone call
    
    * Pong fix
    
    * Playable?
    
    * Merge branch 'main' into caleb
    
    * ALL

---
+++ UntitledPartyGame/Assets/FinishGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishGame : MonoBehaviour
+{
+    public LevelManager lv;
+
+    void Start()
+    {
+        lv = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (LevelManager.currentNoiseLevel <= LevelManager.winNoiseThreshold)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}

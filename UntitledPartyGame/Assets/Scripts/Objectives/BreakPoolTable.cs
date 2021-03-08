0e124f82cbaddd8d448ebaf8724fdcbf23802a10
myersca2024 <71057364+myersca2024@users.noreply.github.com>
2021-03-08 00:51:03 -0600

    Quality of life (#82)
    * Added asset pack and probuilder
    
    * AI changes
    
    * Moved reticle script
    
    * Following bugfix
    
    * AI update
    
    * Organization merge
    
    * Added AI attacking
    
    * Complete AI functionality
    
    * Updated scene
    
    * Police AI update (not finished)
    
    * Prefab Bugfix
    
    * Completely functional AI
    
    * Lot of imports
    
    * Also updated reticle
    
    * Final bugfix to throwing
    
    * Bugfixing
    
    * Water and firepit
    
    * Added furniture pack
    
    * Fine-tuning for level prototype
    
    * Added first level prototype
    
    * Added Chad effect to NPCs
    
    * Re-added NPC SFX
    
    * Updated main menu
    
    * Added much functionality to level
    
    * Update prototype
    
    * Added cracked TV
    
    * TV prefab completed
    
    * Updated AI to be hit by bottles
    
    * Added level functionality
    
    * added game over screen
    
    * Phone call functionality
    
    * Finished phone call
    
    * Pong fix
    
    * Playable?
    
    * Merge branch 'main' into caleb
    
    * ALL
    
    * AFDSAFASDF
    
    * Added quality of life

--- UntitledPartyGame/Assets/Scripts/Objectives/BreakPoolTable.cs
+++ UntitledPartyGame/Assets/Scripts/Objectives/BreakPoolTable.cs
@@ -6,11 +6,35 @@ public class BreakPoolTable : MonoBehaviour
 {
     public int numHits = 5;
     public AudioClip breakPoolTableSFX;
+    public float speedThreshold = 10f;
+    public float explosionForce = 100f;
+    public float explosionRadius = 5f;
+
     LevelManager lv;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         lv = FindObjectOfType<LevelManager>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Break()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.AddComponent<Rigidbody>();
+            child.gameObject.AddComponent<BoxCollider>();
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, child.position, explosionRadius);
+        }
+
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        Destroy(this.gameObject.GetComponent<Rigidbody>());
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +44,7 @@ public class BreakPoolTable : MonoBehaviour
             --numHits;
             if (numHits <= 0)
             {
+                Break();
                 AudioSource.PlayClipAtPoint(breakPoolTableSFX, Camera.main.transform.position);
                 lv.TableComplete();
             }
